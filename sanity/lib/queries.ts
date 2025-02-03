d30ceff84f925692641933bc6da756346f820628
develaparX <srarfian@gmail.com>
2025-02-03 20:07:49 +0700

    author authentication flow & understanding caching

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -19,7 +19,7 @@ export const STARTUP_BY_ID_QUERY = defineQuery(`*[_type=="startup" && _id == $id
     title, 
     slug, 
     _createdAt, 
-    author->{
+    author->{ 
       _id, name,username, image, bio
     }, 
     views,
@@ -31,3 +31,15 @@ export const STARTUP_BY_ID_QUERY = defineQuery(`*[_type=="startup" && _id == $id
 
 export const STARTUP_VIEWS_QUERY = defineQuery(`*[_type == "startup" && _id == $id][0]{
   _id,views}`)
+
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
+  *[_type == "author" && id == $id][0]{
+  _id,
+  id,
+  name,
+  username,
+  email,
+  image,
+  bio
+  }
+  `)
